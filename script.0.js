1fcb4a2486db156645fec244c1ab99838547ffa4
Buyhaut <dukikobalingko@gmail.com>
2024-12-05 21:55:23 +0800

    Add files via upload

---
+++ script.0.js
@@ -0,0 +1,86 @@
+const search = document.querySelector(".search-box input"),
+      images = document.querySelectorAll(".image-box");
+search.addEventListener("keyup", e =>{
+    if(e.key == "Enter"){
+        let searcValue = search.value,
+            value = searcValue.toLowerCase();
+            images.forEach(image =>{
+                if(value === image.dataset.name){ //matching value with getting attribute of images
+				return image.style.display = "block";
+                }
+                image.style.display = "none";
+            });
+    }
+});
+
+search.addEventListener("keyup", () =>{
+    if(search.value != "") return;
+
+    images.forEach(image =>{
+        image.style.display = "block";
+    })
+})
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 6000); // Change image every 5 seconds
+}
+
+function Slider() {
+    const carouselSlides = document.querySelectorAll('.slide');
+    const btnPrev = document.querySelector('.prev');
+    const btnNext = document.querySelector('.next');
+    const dotsSlide = document.querySelector('.dots-container');
+    let currentSlide = 0;
+  
+    const activeDot = function (slide) {
+        document.querySelectorAll('.dot').forEach(dot => dot.classList.remove('active'));
+        document.querySelector(`.dot[data-slide="${slide}"]`).classList.add('active');
+    };
+    activeDot(currentSlide);
+
+    const changeSlide = function (slides) {
+        carouselSlides.forEach((slide, index) => (slide.style.transform = `translateX(${100 * (index - slides)}%)`));
+    };
+    changeSlide(currentSlide);
+
+    btnNext.addEventListener('click', function () {
+        currentSlide++; 
+        if (carouselSlides.length - 1 < currentSlide) {
+            currentSlide = 0;
+        };
+        changeSlide(currentSlide);
+        activeDot(currentSlide);
+});
+    btnPrev.addEventListener('click', function () {
+        currentSlide--;
+        if (0 >= currentSlide) {
+            currentSlide = 0;
+        }; 
+        changeSlide(currentSlide);
+        activeDot(currentSlide);
+    });
+
+    dotsSlide.addEventListener('click', function (e) {
+        if (e.target.classList.contains('dot')) {
+            const slide = e.target.dataset.slide;
+            changeSlide(slide);
+            activeDot(slide);
+        }
+    });
+  };
+Slider();
